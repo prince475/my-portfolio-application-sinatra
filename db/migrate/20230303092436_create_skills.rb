660aada674ee33da7427499b0b3d932264942905
prince475 <danielmwai475@gmail.com>
2023-03-03 12:46:16 +0300

    Updating the createskills migrate folder with the details of our skills table before creating a migration

--- db/migrate/20230303092436_create_skills.rb
+++ db/migrate/20230303092436_create_skills.rb
@@ -1,4 +1,10 @@
 class CreateSkills < ActiveRecord::Migration[7.0]
   def change
+    create_table :skills do
+      t.string :name, null: false
+      t.integer :user_id, null: false
+      t.datetime :created_at, null: false
+      t.timestamps
+    end
   end
 end
