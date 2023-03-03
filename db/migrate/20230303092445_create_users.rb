53f62b56e61f2774b8742d559b7009d312fc5bea
prince475 <danielmwai475@gmail.com>
2023-03-03 12:58:31 +0300

    Updating the createusers migrate folder with the details of our users table before creating a migration

--- db/migrate/20230303092445_create_users.rb
+++ db/migrate/20230303092445_create_users.rb
@@ -1,4 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
+    create_table :users do
+    t.string :name, null: false
+    t.string :email, null: false, index: { unique: true, name: 'uk_email' }
+    t.string :password_hash, null: false
+    t.datetime :createdAt, null: false
+    t.string :prof_pic_url
+    t.integer :phone_number
+    t.string :zip_address
+    t.string :country
+    t.string :profession
+    t.string :education
+    t.integer :experience
+    t.string :skills
+    t.timestamps
+    end
   end
 end
