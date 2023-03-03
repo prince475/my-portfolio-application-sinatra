ec7c2095693eb3f11749ce369f010f7af7097484
prince475 <danielmwai475@gmail.com>
2023-03-03 13:22:12 +0300

    updating all our instance of the do block in the migrate files

--- db/migrate/20230303092423_create_projects.rb
+++ db/migrate/20230303092423_create_projects.rb
@@ -1,6 +1,6 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
-    create_table :projects do
+    create_table :projects do |t|
       t.string :title, null: false
       t.string :description, null: false
       t.integer :status, null: false, default: 0
--- db/migrate/20230303092436_create_skills.rb
+++ db/migrate/20230303092436_create_skills.rb
@@ -1,6 +1,6 @@
 class CreateSkills < ActiveRecord::Migration[7.0]
   def change
-    create_table :skills do
+    create_table :skills do |t|
       t.string :name, null: false
       t.integer :user_id, null: false
       t.datetime :created_at, null: false
