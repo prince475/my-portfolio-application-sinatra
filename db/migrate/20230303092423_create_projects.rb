c335f9361f5f5b61b62480cd32c5b8e21f380b42
prince475 <danielmwai475@gmail.com>
2023-03-03 12:42:00 +0300

    Updating the createprojects migrate folder with the details of our projects table before creating a migration

--- db/migrate/20230303092423_create_projects.rb
+++ db/migrate/20230303092423_create_projects.rb
@@ -1,4 +1,13 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
+    create_table projects do
+      t.string :title, null: false
+      t.string :description, null: false
+      t.integer :status, null: false, default: 0
+      t.integer :user_id
+      t.datetime :createdAt, null: false
+      t.timestamps
+    end
+
   end
 end
